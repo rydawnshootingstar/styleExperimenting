efc42c95768298dc6da0aacd3d5d509b47b73478
rydawnshootingstar <rydawnshootingstar@gmail.com>
2020-01-17 14:53:02 -0500

    commit fuckery. this one is correct.

--- src/components/BookApp/BookApp.js
+++ src/components/BookApp/BookApp.js
@@ -1,38 +1,34 @@
-import React, {Component} from 'react';
-import BookHeader from './BookHeader';
-import BookMain from './BookMain';
-import SideBar from './SideBar';
-import styles from './book-app.scss';
+import React, { Component } from "react";
+import BookHeader from "./BookHeader";
+import BookMain from "./BookMain";
+import SideBar from "./SideBar";
+import styles from "./book-app.scss";
 
-class BookApp extends Component{
-    state={
-        sideBarOpen: false,
-        hasNotifications: true
-    }
+class BookApp extends Component {
+	state = {
+		sideBarOpen: false,
+		hasNotifications: true
+	};
 
-    toggleSideBar = ()=> {
-        console.log('changing bar')
-        this.setState({
-            sideBarOpen: !this.state.sideBarOpen
-        });
-    }
+	toggleSideBar = () => {
+		console.log("changing bar");
+		this.setState({
+			sideBarOpen: !this.state.sideBarOpen
+		});
+	};
 
-    render(){
-        return (
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-            <div className={styles.bookAppContainer}>
-=======
-            <div>
->>>>>>> 355a11f... style links etc
-=======
-            <div className={styles.bookAppContainer}>
->>>>>>> Stashed changes
-                <BookHeader hasNotifications={this.state.hasNotifications} />
-                <BookMain sideBarOpen={this.state.sideBarOpen} toggleSideBar={this.toggleSideBar} closeSideBar={this.closeSideBar}/>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div className={styles.bookAppContainer}>
+				<BookHeader hasNotifications={this.state.hasNotifications} />
+				<BookMain
+					sideBarOpen={this.state.sideBarOpen}
+					toggleSideBar={this.toggleSideBar}
+					closeSideBar={this.closeSideBar}
+				/>
+			</div>
+		);
+	}
 }
 
-export default BookApp; 
+export default BookApp;
--- src/components/BookApp/BookHeader.js
+++ src/components/BookApp/BookHeader.js
@@ -2,21 +2,9 @@ import React, { Component } from "react";
 import styles from "./book-app.scss";
 import { faSearch, faBell } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class BookHeader extends Component {
-=======
-
-class BookHeader extends Component {
-
->>>>>>> 355a11f... style links etc
-=======
-import {Link} from 'react-router-dom';
-
-class BookHeader extends Component {
->>>>>>> Stashed changes
 	render() {
 		return (
 			<div className={styles.header}>
@@ -25,24 +13,13 @@ class BookHeader extends Component {
 					<div className={styles.logoText}>Librarium</div>
 				</div>
 				<div className={styles.list}>
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-					<div className={styles.item}><Link to="/">Home</Link></div>
-=======
-					<div className={styles.item}>Home</div>
->>>>>>> 355a11f... style links etc
-=======
-					<div className={styles.item}><Link to="/">Home</Link></div>
->>>>>>> Stashed changes
+					<div className={styles.item}>
+						<Link to="/">Home</Link>
+					</div>
 					<div className={styles.item}>Library</div>
 					<div className={styles.item}>My Books</div>
 					<div className={styles.item}>Subscribe</div>
 				</div>
-
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-=======
->>>>>>> Stashed changes
 				<div className={styles.headerRightMenu}>
 					<div>
 						<FontAwesomeIcon icon={faSearch} />
@@ -50,16 +27,6 @@ class BookHeader extends Component {
 						<FontAwesomeIcon icon={faBell} />
 					</div>
 				</div>
-<<<<<<< Updated upstream
-=======
-				<div className={this.props.hasNotifications ? styles['headerRightMenu--notifications'] : styles.headerRightMenu}>
-                    <div>
-                        <FontAwesomeIcon icon={faSearch} />
-                    </div>
-                </div>
->>>>>>> 355a11f... style links etc
-=======
->>>>>>> Stashed changes
 			</div>
 		);
 	}
